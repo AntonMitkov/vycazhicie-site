ad4566153fc5e7c3b6995214269f3e1a6804161d
AntonMitkov <mitkovanton1@gmail.com>
2025-07-20 23:28:56 +0300

    Almost all photos

--- src/app/components/secthree.tsx
+++ src/app/components/secthree.tsx
@@ -7,17 +7,17 @@ const SecThree = () => {
             WebkitFilter: 'drop-shadow(0px 0px 30px rgba(255, 255, 255, 1))',
             filter: 'drop-shadow(0px 0px 30px rgba(255, 255, 255, 1))',
         }}>
-        <div className="h-screen bg-newwhite flex flex-col items-center justify-center gap-15 px-5">
+        <div className="h-screen max-[700px]:h-fit bg-newwhite flex flex-col items-center justify-center gap-15 px-5 max-[700px]:py-15">
             <p className="text-4xl min-[768px]:text-6xl font-black text-newblack">Нашы арганізатары</p>
             <div>
                 <div className="flex flex-row gap-5 max-[1200px]:flex-col">
                     <div className="flex flex-row gap-5">
                       <Profile title="Ягор Вератынскі" imageSrc="orgs/vertik.png"/>
-                      <Profile title="Ягор Вератынскі" imageSrc="orgs/vertik.png"/>
+                      <Profile title="Маша Скнарына" imageSrc="orgs/masha_skn_solnyshko.png"/>
                     </div>
                     <div className="flex flex-row gap-5">
-                      <Profile title="Ягор Вератынскі" imageSrc="orgs/vertik.png"/>
-                      <Profile title="Ягор Вератынскі" imageSrc="orgs/vertik.png"/>
+                      <Profile title="Таня Паклонская" imageSrc="orgs/vertik.png"/>
+                      <Profile title="Антон Міцькоў" imageSrc="orgs/AntonMeatCow.png"/>
                     </div>
                 </div>
             </div>

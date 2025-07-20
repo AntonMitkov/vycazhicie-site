95e77633e5218570489aefa1c524534301957b28
AntonMitkov <mitkovanton1@gmail.com>
2025-07-20 18:56:00 +0300

    Removing arrow

--- src/app/components/banner.tsx
+++ src/app/components/banner.tsx
@@ -30,28 +30,6 @@ const Banner = () => {
                     <p className='text-3xl'>і будзьце пачутымі!</p>
                 </div>
                 {/* <DownOutlined style={{ fontSize: '36px', position: 'fixed', bottom: 40 }}/> */}
-                <div style={{position: 'fixed', bottom: 40}}>
-                    <a href="#intro">
-                    <button
-                        className="cursor-pointer bg-newblack px-3 py-2 rounded-md text-white tracking-wider shadow-xl animate-bounce hover:invert"
-                        >
-                        <svg
-                            className="w-5 h-5"
-                            stroke="currentColor"
-                            strokeWidth="2"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <path
-                            d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3"
-                            strokeLinejoin="round"
-                            strokeLinecap="round"
-                            ></path>
-                        </svg>
-                    </button>
-                    </a>
-                </div>
             </div>
         </div>
     </div>

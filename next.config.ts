f6f9525c26a0d2f4e102bbcf47fd16a09fdc696c
AntonMitkov <mitkovanton1@gmail.com>
2025-09-06 16:44:58 +0300

    Little bugfix

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack(config) {
-    // Exclude .svg from Next's default file loader
-    const fileLoaderRule = config.module.rules.find(
-      (rule: any) => rule.test && rule.test.test(".svg")
-    );
-    if (fileLoaderRule) {
-      fileLoaderRule.exclude = /\.svg$/i;
-    }
-
-    // Add SVGR loader
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ["@svgr/webpack"],
-    });
-
-    return config;
-  },
+  // Remove custom webpack configuration
 };
 
 export default nextConfig;
